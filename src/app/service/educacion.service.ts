9a3012790ae66608a95527efb467d49b176d0491
Mailen <juarezmailen@gmail.com>
2022-08-08 00:07:38 -0300

    edu serv comilla

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = "https://primerportfolio.herokuapp.com/educacion/";
+  URL = 'https://primerportfolio.herokuapp.com/educacion/';
 
   constructor(private httpClient: HttpClient) { }
 
